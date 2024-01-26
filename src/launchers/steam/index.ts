18e53d0cf02c856c69caffc8b29cefbf7e56bb36
Tobey Blaber <contact@tobey.me>
2024-01-26 17:59:27 +0000

    Export full enum, not just type information

--- src/launchers/steam/index.ts
+++ src/launchers/steam/index.ts
@@ -1,2 +1,2 @@
-export { SteamApp, type SteamAppManifest, type SteamAppState } from "./app.js";
+export { SteamApp, type SteamAppManifest, SteamAppState } from "./app.js";
 export { SteamLauncher, type SteamLibraryFolders } from "./launcher.js";
