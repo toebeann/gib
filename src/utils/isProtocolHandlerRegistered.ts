48448dcde71b227c7967464bd486f449947b0705
Tobey Blaber <contact@tobey.me>
2024-01-27 00:25:44 +0000

    Make `isProtocolHandlerRegistered` cross-platform

--- src/utils/isProtocolHandlerRegistered.ts
+++ src/utils/isProtocolHandlerRegistered.ts
@@ -1,80 +1,13 @@
-import { homedir } from "node:os";
-import { join } from "node:path";
-import { match, P } from "ts-pattern";
-import { z } from "zod";
-import { exec } from "../fs/exec.js";
-
-const launchServicesHandlersSchema = z.object({
-  LSHandlerURLScheme: z.string().optional(),
-}).passthrough().array();
-
-export const launchServicesSecureSchema = z.union([
-  launchServicesHandlersSchema,
-  z.object({
-    LSHandlers: launchServicesHandlersSchema,
-  }).passthrough(),
-]);
-
-const parseLaunchServices = (
-  launchServices: z.infer<typeof launchServicesSecureSchema>,
-) =>
-  match(launchServices)
-    .returnType<z.infer<typeof launchServicesHandlersSchema>>()
-    .with(P.array(), (launchServices) => launchServices)
-    .otherwise((obj) => obj.LSHandlers);
+import ProtocolRegistry from "protocol-registry";
+import { match } from "ts-pattern";
+import { isProtocolHandlerRegistered as win } from "./windows/isProtocolHandlerRegistered.js";
 
 /**
  * Determines whether a handler for a given protocol is registered.
  *
- * Checks the launch services registered in
- * `$HOME/Library/Preferences/com.apple.LaunchServices/com.apple.launchservices.secure.plist`
- * first, and if not found there falls back checking with `open`, which opens
- * the handler for the given protocol in the background if one is registered.
- *
  * @param protocol The protocol to check for.
  */
-export const isProtocolHandlerRegistered = async (protocol: string) => {
-  let plutilOutput: string | undefined;
-  try {
-    const { stdout } = await exec(
-      `plutil -convert json -o - "${
-        join(
-          homedir(),
-          "Library",
-          "Preferences",
-          "com.apple.LaunchServices",
-          "com.apple.launchservices.secure.plist",
-        )
-      }"`,
-    );
-    plutilOutput = stdout.trim();
-  } catch {}
-
-  if (plutilOutput) {
-    const launchServices = launchServicesHandlersSchema.safeParse(
-      JSON.parse(plutilOutput),
-    );
-    if (
-      launchServices.success &&
-      parseLaunchServices(launchServices.data).map((entry) =>
-        entry.LSHandlerURLScheme
-      ).filter(
-        Boolean,
-      ).includes(protocol)
-    ) {
-      return true;
-    }
-  }
-
-  try {
-    await exec(`open --background ${protocol}://`);
-    return true;
-  } catch {
-    try {
-      await exec(`open ${protocol}://`);
-      return true;
-    } catch {
-      return false;
-    }
-  }
-};
+export const isProtocolHandlerRegistered = match(process.platform)
+  .returnType<(protocol: string) => Promise<boolean>>()
+  .with("win32", () => (protocol) => Promise.resolve(win(protocol)))
+  .otherwise(() => ProtocolRegistry.checkifExists);
