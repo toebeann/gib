5e95ed3bda4e92c374a5563b9d34daebc55abaeb
Tobey Blaber <contact@tobey.me>
2024-08-09 07:46:01 +0100

    Revert "further bunx test"
    This reverts commit 366f9cabd4a0d5c72294592f9584c30250c0fa84.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --experimental-strip-types --no-warnings=ExperimentalWarning
 /**
  * This file is gib: a node script which aims to automate installing BepInEx
  * to a Unity game.
