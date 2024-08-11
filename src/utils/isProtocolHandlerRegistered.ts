40c4fabaa488c837d7b1020bdb62d1becce6bbe6
Tobey Blaber <contact@tobey.me>
2024-08-11 16:34:06 +0100

    Simplify `isProtocolHandlerRegistered`

--- src/utils/isProtocolHandlerRegistered.ts
+++ src/utils/isProtocolHandlerRegistered.ts
@@ -1,13 +1,11 @@
 import ProtocolRegistry from "protocol-registry";
-import { match } from "ts-pattern";
-import { isProtocolHandlerRegistered as win } from "./windows/isProtocolHandlerRegistered.ts";
+import { isProtocolHandlerRegistered as win32 } from "./windows/isProtocolHandlerRegistered.ts";
 
 /**
  * Determines whether a handler for a given protocol is registered.
  *
  * @param protocol The protocol to check for.
  */
-export const isProtocolHandlerRegistered = match(process.platform)
-  .returnType<(protocol: string) => Promise<boolean>>()
-  .with("win32", () => (protocol) => Promise.resolve(win(protocol)))
-  .otherwise(() => ProtocolRegistry.checkifExists);
+export const isProtocolHandlerRegistered = process.platform === "win32"
+  ? (protocol: string) => Promise.resolve(win32(protocol))
+  : ProtocolRegistry.checkifExists;
