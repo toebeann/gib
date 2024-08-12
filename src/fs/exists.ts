22a6820e943685561f92b0f41a064c165fdec564
Tobey Blaber <contact@tobey.me>
2024-08-12 14:54:09 +0100

    Simplify `fs/exists`

--- src/fs/exists.ts
+++ src/fs/exists.ts
@@ -4,11 +4,6 @@ import { access } from "node:fs/promises";
  * Determines whether the given path exists in the file system.
  * @param path
  */
-export const exists = async (path: string) => {
-  try {
-    await access(path);
-    return true;
-  } catch {
-    return false;
-  }
-};
+
+export const exists = async (path: string) =>
+  access(path).then((_) => true).catch((_) => false);
