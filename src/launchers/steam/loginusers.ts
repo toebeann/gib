6c894f9b0b7ad2204fc4ab9f287ba1b2f7bc79c0
Tobey Blaber <contact@tobey.me>
2024-08-15 17:34:00 +0100

    Add utilities for working with Steam's `loginusers.vdf` file

---
+++ src/launchers/steam/loginusers.ts
@@ -0,0 +1,107 @@
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { ID } from "@node-steam/id";
+import { parse } from "@node-steam/vdf";
+import { match, P } from "ts-pattern";
+import { z } from "zod";
+import { numericBooleanSchema } from "../../utils/zod.ts";
+import { getLoginUsersPath, getSteamPath } from "./path.ts";
+
+/** Zod schema for working with Steam's `loginusers.vdf` file. */
+export const loginusersSchema = z.object({
+  users: z.record(
+    z.object({
+      AccountName: z.string(),
+      PersonaName: z.unknown().optional(),
+      RememberPassword: numericBooleanSchema.optional(),
+      WantsOfflineMode: numericBooleanSchema.optional(),
+      SkipOfflineModeWarning: numericBooleanSchema.optional(),
+      AllowAutoLogin: numericBooleanSchema.optional(),
+      MostRecent: numericBooleanSchema,
+      Timestamp: z.number().optional(),
+    }).passthrough(),
+  ),
+});
+
+/** Steam's `loginusers.vdf` file, parsed. */
+export type LoginUsers = z.infer<typeof loginusersSchema>;
+
+/**
+ * Gets inforation about users who have logged in to the Steam app on this
+ * computer, parsed from Steam's `loginusers.vdf` file.
+ */
+export const getUsers = () =>
+  readFile(
+    getLoginUsersPath(),
+    { encoding: "utf8" },
+  ).then((text) => Object.entries(loginusersSchema.parse(parse(text)).users));
+
+/**
+ * Retrieves information about the most recent user of the Steam app on this
+ * computer, parsed from Steam's `loginusers.vdf` file.
+ */
+export const getMostRecentUser = () =>
+  getUsers()
+    .then((users) => users.find(([_, user]) => user.MostRecent));
+
+/**
+ * Attempts to determine the path to the `config` folder for the most recent
+ * Steam app user on this computer. Returns `undefined` if no users have logged
+ * in to the Steam app on this computer.
+ *
+ * @returns The path to the `config` folder for the most recent Steam app user
+ * on this computer, or `undefined` if a user could not be found.
+ */
+export function getUserConfigFolderPath(): Promise<string | undefined>;
+
+/**
+ * Determines the path to the `config` folder for the Steam user matching
+ * `userId`.
+ *
+ * @param userId ID of the Steam user.
+ */
+export function getUserConfigFolderPath(userId: ID): Promise<string>;
+
+/***
+ * Determines the path to the `config` folder for the Steam user with id
+ * `userId`.
+ *
+ * @param userId ID of the Steam user.
+ */
+export function getUserConfigFolderPath(userId: string): Promise<string>;
+
+/**
+ * Determines the path to the `config` folder for a Steam user on this
+ * computer.
+ *
+ * @param userId ID of the steam user. If not specified, will attempt to look
+ * up the most recent user of the Steam app on this computer.
+ *
+ * @returns The path to the `config` folder for the relevant user, or
+ * `undefined` if no user ID specified and a user of the Steam app could not be
+ * found on this computer.
+ */
+export function getUserConfigFolderPath(
+  userId?: ID | string,
+): Promise<string | undefined>;
+
+export async function getUserConfigFolderPath(userId?: ID | string) {
+  const { accountid } = await match(userId)
+    .returnType<Promise<ID | undefined>>()
+    .with(P.instanceOf(ID), (id) => Promise.resolve(id))
+    .with(P.string, (id) => Promise.resolve(new ID(id)))
+    .otherwise(() =>
+      getMostRecentUser()
+        .then((user) => user?.[0] && new ID(user?.[0]) || undefined)
+    )
+    .then((id) => id || { accountid: undefined });
+
+  if (!accountid) return;
+
+  return join(
+    getSteamPath(),
+    "userdata",
+    accountid.toString(),
+    "config",
+  );
+}
