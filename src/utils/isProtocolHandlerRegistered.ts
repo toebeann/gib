747240122fdffbdfe99388ccf4211efc0b89dd1c
Tobey Blaber <contact@tobey.me>
2024-08-18 16:55:04 +0100

    rm prod dep: `registry-js`

--- src/utils/isProtocolHandlerRegistered.ts
+++ src/utils/isProtocolHandlerRegistered.ts
@@ -1,11 +1,8 @@
 import ProtocolRegistry from "protocol-registry";
-import { isProtocolHandlerRegistered as win32 } from "./windows/isProtocolHandlerRegistered.ts";
 
 /**
  * Determines whether a handler for a given protocol is registered.
  *
  * @param protocol The protocol to check for.
  */
-export const isProtocolHandlerRegistered = process.platform === "win32"
-  ? (protocol: string) => Promise.resolve(win32(protocol))
-  : ProtocolRegistry.checkifExists;
+export const isProtocolHandlerRegistered = ProtocolRegistry.checkifExists;
