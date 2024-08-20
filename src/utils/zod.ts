b8f821a037554947be19ad425ba21e76f825734f
Tobey Blaber <contact@tobey.me>
2024-08-20 17:53:27 +0100

    Import only specific lodash modules that are used

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -1,9 +1,10 @@
-import _ from "lodash";
+import camelCase from "lodash.camelcase";
+import mapKeys from "lodash.mapkeys";
 import { z } from "zod";
 
 export const numericBooleanSchema = z.union([z.literal(0), z.literal(1)]);
 
 export const toCamelCaseKeys =
   z.record(z.unknown()).transform((obj) =>
-    _.mapKeys(obj, (__, key) => _.camelCase(key))
+    mapKeys(obj, (__, key) => camelCase(key))
   ).pipe;
