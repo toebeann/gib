564049dbc09548ae822dceb2a33c6b38f61003d7
Tobey Blaber <contact@tobey.me>
2024-08-28 03:20:19 +0100

    Use bun shell instead of node exec

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -77,7 +77,6 @@ import unquote from "unquote";
 import wrapAnsi from "wrap-ansi";
 import { z } from "zod";
 import { renderLogo } from "./renderLogo.ts";
-import { exec } from "../fs/exec.ts";
 import { exists } from "../fs/exists.ts";
 import { getAppById, getAppsByPath, launch } from "../launchers/steam/app.ts";
 import { isInstalled } from "../launchers/steam/launcher.ts";
@@ -93,6 +92,7 @@ import { getFixedPath } from "../utils/getFixedPath.ts";
 import { setLaunchOptions } from "../launchers/steam/launchOption.ts";
 import { getMostRecentUser } from "../launchers/steam/loginusers.ts";
 import { parsePlistFromFile, type Plist } from "../utils/plist.ts";
+import { $ } from "bun";
 
 export const run = async () => {
   function alertShim(message: string) {
@@ -846,25 +846,16 @@ export const run = async () => {
               const { CFBundleIconFile, CFBundleName } = plist;
               if (CFBundleIconFile) {
                 return Promise.all([
-                  exec(quote([
-                    "sips",
-                    "-s",
-                    "format",
-                    "png",
-                    join(
-                      gameAppPath,
-                      "Contents",
-                      "Resources",
-                      CFBundleIconFile,
-                    ),
-                    "--out",
+                  $`sips -s format png ${
+                    join(gameAppPath, "Contents", "Resources", CFBundleIconFile)
+                  } --out ${
                     join(
                       shortcutPath,
                       "Contents",
                       "Resources",
                       "PlayerIcon.png",
-                    ),
-                  ])),
+                    )
+                  }`.quiet(),
                   copyFile(
                     join(
                       gameAppPath,
@@ -1052,15 +1043,11 @@ export const run = async () => {
               await ensureDir(join(shortcutPath, "Contents", "Resources"));
               await Promise.all([
                 shouldAddShortcut &&
-                exec(quote([
-                  "sips",
-                  "-s",
-                  "format",
-                  "png",
-                  join(gameAppPath, "Contents", "Resources", CFBundleIconFile),
-                  "--out",
-                  join(shortcutPath, "Contents", "Resources", "PlayerIcon.png"),
-                ])),
+                $`sips -s format png ${
+                  join(gameAppPath, "Contents", "Resources", CFBundleIconFile)
+                } --out ${
+                  join(shortcutPath, "Contents", "Resources", "PlayerIcon.png")
+                }`.quiet(),
                 copyFile(
                   join(gameAppPath, "Contents", "Resources", CFBundleIconFile),
                   join(
