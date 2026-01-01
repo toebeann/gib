86ecc7c59956281b8fa01c465b961ffc1a95e240
Tobey Blaber <contact@tobey.me>
2026-01-01 05:22:11 +0000

    refactor: move steam path helper implementations

--- src/launchers/steam/path.ts
+++ src/launchers/steam/path.ts
@@ -1,25 +1,36 @@
+import { homedir } from "node:os";
+import { join } from "node:path";
 import { platform } from "node:process";
-
-import {
-  getLibraryFoldersPath as getLibraryFoldersPathMac,
-  getLoginUsersPath as getLoginUsersPathMac,
-  getSteamPath as getSteamPathMac,
-} from "./macos/path.ts";
 
 /** Retrieves the path to the Steam installation. */
 export const getSteamPath = () => {
-  if (platform === "darwin") return getSteamPathMac();
+  if (platform === "darwin") {
+    return join(
+      homedir(),
+      "Library",
+      "Application Support",
+      "Steam",
+    );
+  }
   return "";
 };
 
 /** Retrieves the path to Steam's `libraryfolders.vdf` file. */
 export const getLibraryFoldersPath = () => {
-  if (platform === "darwin") return getLibraryFoldersPathMac();
+  if (platform === "darwin") {
+    return join(
+      getSteamPath(),
+      "config",
+      "libraryfolders.vdf",
+    );
+  }
   return "";
 };
 
 /** Retrieves the path to Steam's `loginusers`.vdf` file. */
 export const getLoginUsersPath = () => {
-  if (platform === "darwin") return getLoginUsersPathMac();
+  if (platform === "darwin") {
+    return join(getSteamPath() ?? homedir(), "config", "loginusers.vdf");
+  }
   return "";
 };
