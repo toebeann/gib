840043a512b74264b92e6727ff93b5464fd7a5a5
Tobey Blaber <contact@tobey.me>
2026-01-01 03:11:19 +0000

    fix: update `z.record` usage for zod v4

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -5,6 +5,6 @@ import { z } from "zod";
 export const numericBooleanSchema = z.union([z.literal(0), z.literal(1)]);
 
 export const toCamelCaseKeys =
-  z.record(z.unknown()).transform((obj) =>
+  z.record(z.string(), z.unknown()).transform((obj) =>
     mapKeys(obj, (__, key) => camelCase(key))
   ).pipe;
