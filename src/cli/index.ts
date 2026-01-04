6b280c3dc565fa01f644d170ff0da7a14563d52e
Tobey Blaber <contact@tobey.me>
2026-01-04 21:28:03 +0000

    style: organise argv parsing

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1401,38 +1401,18 @@ export const prerun = async () => {
     },
   } = parseArgs({
     args: argv,
-    options: {
-      help: {
-        type: "boolean",
-        short: "h",
-        default: false,
-      },
-      version: {
-        type: "boolean",
-        short: "v",
-        default: false,
-      },
-      status: {
-        type: "boolean",
-        short: "s",
-        default: false,
-      },
-      update: {
-        type: "boolean",
-        default: true,
-      },
-      ...(command === "gib"
-        ? {
-          "path-check": {
-            type: "boolean",
-            default: true,
-          },
-        }
-        : {}),
-    },
-    strict: true,
     allowNegative: true,
     allowPositionals: true,
+    strict: true,
+    options: {
+      help: { type: "boolean", short: "h", default: false },
+      version: { type: "boolean", short: "v", default: false },
+      status: { type: "boolean", short: "s", default: false },
+      update: { type: "boolean", default: true },
+      ...(command === "gib"
+        ? { "path-check": { type: "boolean", default: true } }
+        : {}),
+    },
   });
 
   const printHelp = () => {
